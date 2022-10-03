dffbd8f611b15426090fde6ad7aa99abfa15a4ab
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-03 15:06:56 +0800

    AM 01st commit

---
+++ pro08-maven-module/src/main/java/com/itheima/maven_test/Maven_test.java
@@ -0,0 +1,15 @@
+package com.itheima.maven_test;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> pro07-maven-parent
+ * BelongsPackage ==> com.itheima.maven_test
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-02 20:22:10
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+public class Maven_test {
+    public static void main(String [] args){
+        System.out.println("hello,maven..");
+    }
+}
